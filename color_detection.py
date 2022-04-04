fe1a4f1213311f1f04736ebca8bb5b653945911b
Rhodri <24809571+rhld16@users.noreply.github.com>
2022-04-05 00:51:17 +0100

    Update color_detection.py

--- color_detection.py
+++ color_detection.py
@@ -5,23 +5,39 @@ from PIL import Image
 im = Image.open('unknown.png')
 colors = {}
 
+def get_dominant_color(pil_img, palette_size=16):
+    # Resize image to speed up processing
+    img = pil_img.copy()
+    img.thumbnail((100, 100))
+
+    # Reduce colors (uses k-means internally)
+    paletted = img.convert('P', palette=Image.ADAPTIVE, colors=palette_size)
+
+    # Find the color that occurs most often
+    palette = paletted.getpalette()
+    color_counts = sorted(paletted.getcolors(), reverse=True)
+    palette_index = color_counts[0][1]
+    dominant_color = palette[palette_index*3:palette_index*3+3]
+
+    return dominant_color
+
 def split_into_rows(im, row_height, col_width):
     y = 0
     while y < row_height:
         x = 0
-        print(y, x,)
-        print( x < col_width)
         while x < col_width:
-            print(f'x: {x}, y: {y}')
             top_left = ((x/col_width)*im.width, (y/row_height)*im.height)
             bottom_right = (min((x/col_width)*im.width + 1/col_width*im.width, im.width), min((y/row_height)*im.height + 1/row_height*im.height, im.height))
             image = im.crop((*top_left, *bottom_right))
-            save_path = os.path.join(OUTPUT_DIR, f'{x}{y}.png')
+            save_path = os.path.join(OUTPUT_DIR, f'{x+67},{y+115}.png')
 
             image.save(save_path)
-            color = image.getpixel((image.width/2, image.height/2))
+            color = get_dominant_color(image)
+            if (x < 81-67 and y < 137-115):
+                x += 1
+                continue
             hex = "{0:02x}{1:02x}{2:02x}".format(color[0], color[1], color[2])
-            colors[f'{x+74},{y+117}'] = hex
+            colors[f'{x+67},{y+115}'] = hex
             yield image
 
             x += 1
@@ -30,20 +46,8 @@ def split_into_rows(im, row_height, col_width):
 
 OUTPUT_DIR = 'output'
 
-for i, row in enumerate(split_into_rows(im, 30, 52)):
+for i, row in enumerate(split_into_rows(im, 32+1, 63+1)):
     save_path = os.path.join(OUTPUT_DIR, f'{i}.png')
 
-    row.save(save_path)
-
-# dump colors as json to file
 with open('colors.json', 'w') as f:
     json.dump(colors, f)
-
-# array of all coords in colors that have hex e50000
-red_pixels = []
-for coord, hex in colors.items():
-    if hex == 'e50000':
-        red_pixels.append(coord)
-
-with open('red.json', 'w') as f:
-    json.dump(red_pixels, f)
